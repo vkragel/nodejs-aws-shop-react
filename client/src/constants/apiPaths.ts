205fa9d3bd77e89721b49fc061a81398a025d83b
v.kragel <v.kragel@softteco.com>
2025-03-12 15:15:28 +0300

    refactor: change client api urls

--- client/src/constants/apiPaths.ts
+++ client/src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://f0cud14cxh.execute-api.us-east-2.amazonaws.com/dev",
+  product: "https://1v888w2r3g.execute-api.us-east-2.amazonaws.com/dev",
   order: "https://.execute-api.us-east-2.amazonaws.com/dev",
-  import: "https://c36o8ccoal.execute-api.us-east-2.amazonaws.com/dev",
+  import: "https://g3a4px8ukf.execute-api.us-east-2.amazonaws.com/dev",
   bff: "https://.execute-api.us-east-2.amazonaws.com/dev",
   cart: "https://.execute-api.us-east-2.amazonaws.com/dev",
 };
