ab22e8d3fb5ce04981dd1748d550cc1f4d264f9b
v.kragel <v.kragel@softteco.com>
2025-03-12 22:47:09 +0300

    refactor: update api urls

--- client/src/constants/apiPaths.ts
+++ client/src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://1v888w2r3g.execute-api.us-east-2.amazonaws.com/dev",
+  product: "https://cjkuynh9rf.execute-api.us-east-2.amazonaws.com/dev",
   order: "https://.execute-api.us-east-2.amazonaws.com/dev",
-  import: "https://g3a4px8ukf.execute-api.us-east-2.amazonaws.com/dev",
+  import: "https://nvfxbvuc17.execute-api.us-east-2.amazonaws.com/dev",
   bff: "https://.execute-api.us-east-2.amazonaws.com/dev",
   cart: "https://.execute-api.us-east-2.amazonaws.com/dev",
 };
