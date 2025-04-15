9df6d8ecc6c24a24f2ed82da23dd1d0553429f13
v.kragel <v.kragel@softteco.com>
2025-04-15 14:59:17 +0300

    update project

--- client/src/constants/apiPaths.ts
+++ client/src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
   product: "https://cjkuynh9rf.execute-api.us-east-2.amazonaws.com/dev",
   order:
-    "http://vkragel-cart-api-development.eu-west-1.elasticbeanstalk.com/api/profile/cart",
+    "https://wnfn6gizvkofsjdw67rzpwp47y0dleph.lambda-url.us-east-2.on.aws/api/profile/cart",
   import: "https://nvfxbvuc17.execute-api.us-east-2.amazonaws.com/dev",
   bff: "https://.execute-api.us-east-2.amazonaws.com/dev",
-  cart: "http://vkragel-cart-api-development.eu-west-1.elasticbeanstalk.com/api",
+  cart: "https://wnfn6gizvkofsjdw67rzpwp47y0dleph.lambda-url.us-east-2.on.aws/api",
 };
 
 export default API_PATHS;
